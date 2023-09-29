9a163504875c8bf01c54af3c9aa68eb500fd79a3
Rahulraymond <63040146+Rahulraymond@users.noreply.github.com>
2023-09-29 10:51:08 +0530

    Login page name updated to Kshipra path update

--- resources/views/include/css.blade.php
+++ resources/views/include/css.blade.php
@@ -1,14 +1,14 @@
 <!-- core:css -->
-<link rel="stylesheet" href="{{ asset(public/assets/vendors/core/core.css') }}">
+<link rel="stylesheet" href="{{ asset('public/assets/vendors/core/core.css') }}">
 <!-- plugin css for this page -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <!-- end plugin css for this page -->
 
-<link rel="stylesheet" href="{{ asset(public/assets/css/style.css') }}">
-<link rel="stylesheet" href="{{ asset(public/assets/css/custom.css') }}">
+<link rel="stylesheet" href="{{ asset('public/assets/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('public/assets/css/custom.css') }}">
 <!-- End layout styles
-<link rel="shortcut icon" href="{{ asset(public/assets/images/fav.png') }}" />-->
+<link rel="shortcut icon" href="{{ asset('public/assets/images/fav.png') }}" />-->
 
 <link rel="shortcut icon" href="https://cleverapp.in/pe/assets/images/lock.png" />
-<link rel="stylesheet" href="{{asset(public/assets/izitoast/css/iziToast.min.css')}}">
+<link rel="stylesheet" href="{{asset('public/assets/izitoast/css/iziToast.min.css')}}">
 @stack('css')
